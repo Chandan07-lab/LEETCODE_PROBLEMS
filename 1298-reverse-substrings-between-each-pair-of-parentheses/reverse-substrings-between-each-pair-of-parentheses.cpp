cbeaa0e1c111e28c359718106c578085fed79b6f
Chandan Kumar jena <116368780+Chandan07-lab@users.noreply.github.com>
2024-07-11 12:37:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (70.28%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -28,3 +28,21 @@ public:
         return result;
     }
 };
+//o(n*n)
+// string reverseParentheses(string s) {
+//         stack<int> openeBracket; //You can use a vector also as stack
+//         string result;
+//         for (char currentChar : s) {
+//             if (currentChar == '(') {
+//                 openeBracket.push(result.length());
+//             } else if (currentChar == ')') {
+//                 int start = openeBracket.top();
+//                 openeBracket.pop();
+//                 reverse(result.begin() + start, result.end());
+//             } else {
+//                 result += currentChar;
+//             }
+//         }
+//         return result;
+//     }
+// };
