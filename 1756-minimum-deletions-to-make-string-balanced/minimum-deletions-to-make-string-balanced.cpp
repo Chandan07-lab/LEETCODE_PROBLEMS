9cf250f632820693fcaef563758f82872da91738
Chandan Kumar jena <116368780+Chandan07-lab@users.noreply.github.com>
2024-07-30 11:34:03 +0530

    Time: 85 ms (68.90%) | Memory: 23.7 MB (73.37%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -21,3 +21,18 @@ public:
         
     }
 };
+// int minimumDeletions(string s) {
+//         int n=s.length();
+//         int count = 0;
+//         stack<char> s;
+// for(int i=0;i<n;i++){
+//     if(!s.empty()&&s[i]=='a'&&s.top()++'b'){
+//         s.pop();
+//         count++;
+//     }else{
+//         s.push(s[i]);
+//     }
+// }
+// return count;
+// }
+// };
