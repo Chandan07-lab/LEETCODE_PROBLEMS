cab1f710a242f331a58ec55213772fd40f686c0f
Chandan Kumar jena <116368780+Chandan07-lab@users.noreply.github.com>
2024-08-04 17:25:13 +0530

    Time: 89 ms (67.87%) | Memory: 27.3 MB (31.24%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -19,3 +19,29 @@ int M=1e9+7;
         return result;
     }
 };
+
+// typedef pair<int,int>p;
+// int m=1e9+7;
+//  int rangeSum(vector<int>& nums, int n, int left, int right){
+//     priority_queue<p,vector<p>,greater<p>> pq;
+
+//     for(int i=0;i<n;i++){
+//         pq.push({nums[i],i});
+//     }
+//     int result=0;
+//     for(int i=1;i<right;i++){
+//         auto p=pq.top;
+//         pq.pop();
+
+//         if(i>=left){
+//             result=(result+p.first)%m;
+//         }
+
+//         if(p.second<n-1){
+//             p.second++;
+//             p.first+=nums[p.second];
+//             pq.push(p);
+//         }
+//     }
+//     return result;
+//  }
