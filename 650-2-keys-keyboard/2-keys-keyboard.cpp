0484eb8f7a7f16eec77f36d4331034d78cac50cd
Chandan Kumar jena <116368780+Chandan07-lab@users.noreply.github.com>
2024-08-19 17:58:04 +0530

    Time: 17 ms (31.47%) | Memory: 12 MB (24.18%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -24,3 +24,66 @@ int solve(int cura,int pca,int n){
         return 1+solve(1,1,n);
     }
 };
+
+// int minSteps(int n){
+//     if(n==1)
+//     return 0;
+
+//     if(n==2)
+//     return 2;
+
+// vector<int> t(n+1,0);
+// t[o]=0;
+// t[1]=0;
+// t[2]=2;
+
+// for(int i=3;i<=3;i++){
+//     int factor=i/2;
+//     while(factor>=1){
+//         if(i%factor==0){
+//             int strf=t[factor];
+//             int ctf=1;
+//             int pf=(i/factor)-1;
+
+//             t[i]=t[factor] + ctf+pf;
+//             break;
+//         }
+//         factor--;
+//     }
+// }
+// return t[n];
+// }
+//Approach-3 (Greedy)
+//T.C : O(n)
+//S.C : O(1)
+// class Solution {
+// public:
+//     int minSteps(int n) {
+//         if(n == 1)
+//             return 0;
+// int remaining = n; //initially
+//         int buffer    = 0;
+//         int count_A   = 1; //initially we have 1 'A'
+//         int steps     = 0;
+// while (count_A < n) {
+//             remaining = n - count_A;
+//             if (remaining % count_A == 0) {
+//                 buffer = count_A;
+//                 count_A *= 2;
+//                 steps += 2; // copy + paste
+//             } else {
+//                 count_A += buffer;
+//                 steps++;
+//             }
+//         }
+        
+//         return steps;
+//     }
+// }
+
+
+
+
+
+
+
