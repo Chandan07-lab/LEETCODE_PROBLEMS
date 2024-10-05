bc644a5be983200c0e61374195b270ab12e49e3b
Chandan Kumar jena <116368780+Chandan07-lab@users.noreply.github.com>
2024-10-05 12:37:03 +0530

    Time: 3 ms (94.61%) | Memory: 8.9 MB (42.93%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -6,13 +6,33 @@ public:
         if(n>m){
             return false;
         }
-        string sorts1=s1;
-        sort(sorts1.begin(),sorts1.end());
-        for(int i=0;i<=m-n;i++){
-            string temp=s2.substr(i,n);
-            sort(temp.begin(),temp.end());
-            if(temp==sorts1) return true;
+        vector<int>s1freq(26,0);
+        vector<int>s2freq(26,0);
+        for(int i=0;i<n;i++){
+            s1freq[s1[i]-'a']++;
         }
-        return false;
+        int i=0;
+        int j=0;
+        while(j<m){
+            s2freq[s2[j]-'a']++;
+
+            if(j-i+1>n){
+                s2freq[s2[i]-'a']--;
+                i++;
+            }
+            if(s1freq==s2freq){
+                return true;
+            }
+            j++;
+        }
+
+        // string sorts1=s1;
+        // sort(sorts1.begin(),sorts1.end());
+        // for(int i=0;i<=m-n;i++){
+        //     string temp=s2.substr(i,n);
+        //     sort(temp.begin(),temp.end());
+        //     if(temp==sorts1) return true;
+        // }
+         return false;
     }
 };
