195c4b2ac3dfe789a6d4f84ecb43041bcdee3a4b
Chandan Kumar jena <116368780+Chandan07-lab@users.noreply.github.com>
2024-10-07 16:02:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (99.28%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -2,26 +2,44 @@ class Solution {
 public:
     int minLength(string s) {
         int n=s.length();
-        stack<int>st;
-        for(int i=0;i<n;i++){
-            char currchar=s[i];
-            if(st.empty()){
-                st.push(currchar);
-                continue;
-            }
+        // stack<int>st;
+        // for(int i=0;i<n;i++){
+        //     char currchar=s[i];
+        //     if(st.empty()){
+        //         st.push(currchar);
+        //         continue;
+        //     }
             
 
-            if(currchar=='B'&& st.top()=='A'){
-                st.pop();
-            }
-            else if(currchar=='D'&&st.top()=='C'){
-                st.pop();
+        //     if(currchar=='B'&& st.top()=='A'){
+        //         st.pop();
+        //     }
+        //     else if(currchar=='D'&&st.top()=='C'){
+        //         st.pop();
+        //     }else{
+        //         st.push(currchar);
+        //     }
+
+
+        // }
+        // return st.size();
+        int i=0;
+        int j=1;
+        while(j<n){
+            if(i<0){
+                i++;
+                s[i]=s[j];
+            }else if((s[i]=='A'&&s[j]=='B')||(s[i]=='C'&&s[j]=='D')){
+                i--;
             }else{
-                st.push(currchar);
+                i++;
+                s[i]=s[j];
             }
-
-
+j++;
         }
-        return st.size();
+
+
+
+return i+1;
     }
 };
