cde457fbaf5ae206d10e23f8ce8799420218a3ab
Chandan Kumar jena <116368780+Chandan07-lab@users.noreply.github.com>
2024-11-01 12:17:10 +0530

    Time: 143 ms (21.21%) | Memory: 68.6 MB (11.04%) - LeetSync

--- 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
+++ 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
@@ -1,31 +1,40 @@
 class Solution {
 public:
+typedef long long ll ;
+ll solve(int ri,int fi,vector<int>&robot,vector<int>& positions,vector<vector<ll>>& t){
+    if(ri >=robot.size()){
+        return 0;
+    }
+    if(fi>=positions.size()){
+          return 1e12;  
+    }
+    if(t[ri][fi]!=-1){
+        return t[ri][fi];
+    }
+    ll takecurrent=abs(robot[ri]-positions[fi]) +solve(ri+1,fi+1,robot,positions,t);
+    ll skip=solve(ri,fi+1,robot,positions,t);
+
+    return t[ri][fi]=min(takecurrent,skip);
+}
     long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
         sort(robot.begin(), robot.end()); 
         sort(factory.begin(), factory.end()); 
 
         int r = robot.size(), f = factory.size();
-        
-        
-        vector<vector<long long>> dp(r + 1, vector<long long>(f + 1, LLONG_MAX / 2));
-        
-        dp[0][0] = 0; 
+        vector<int>positions;
 
-        for (int j = 1; j <= f; ++j) { 
-            dp[0][j] = 0; 
-            for (int i = 1; i <= r; ++i) { 
-                long long totalDist = 0;
-                for (int k = 0; k <= min(i, factory[j - 1][1]); ++k) { // Consider k robots for factory j
-                    if (k > 0) {
-                        totalDist += abs(robot[i - k] - factory[j - 1][0]); // Distance for each of k robots
-                    }
-                    if (dp[i - k][j - 1] < LLONG_MAX / 2) { // Only add if it's a valid (non-overflowing) value
-                        dp[i][j] = min(dp[i][j], dp[i - k][j - 1] + totalDist); // Minimize total distance
-                    }
-                }
+        for(int i=0;i<f;i++){
+            int limit=factory[i][1];
+            int pos=factory[i][0];
+
+            for(int j=0;j<limit;j++){
+                positions.push_back(pos);
             }
         }
-        
-        return dp[r][f]; // Minimum distance for all robots and factories
+        int n=positions.size();
+        vector<vector<ll>>t(r+1,vector<ll>(n+1,-1));
+
+        return solve(0,0,robot,positions,t);
+ 
     }
 };
