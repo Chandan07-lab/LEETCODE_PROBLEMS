cec10204497310a92d68bce28f5ffc39658bdca6
Chandan Kumar jena <116368780+Chandan07-lab@users.noreply.github.com>
2024-11-27 15:40:44 +0530

    Time: 1948 ms (5.24%) | Memory: 605.4 MB (5.53%) - LeetSync

--- 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.cpp
+++ 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.cpp
@@ -1,42 +1,111 @@
 class Solution {
 public:
-int bfs(int n,unordered_map<int,vector<int>>adj){
-    queue<int>q;
-    q.push(0);
-    vector<bool>vis(n,false);
-    vis[0]=true;
-    int level=0;
-    while(!q.empty()){
-        int qsize=q.size();
-        while(qsize--){
-            int node=q.front();
-            q.pop();
-            if(node==n-1){
-                return level;
-            }
-            for(auto&ngbr:adj[node]){
-                if(!vis[ngbr]){
-                    q.push(ngbr);
-                    vis[ngbr]=true;
-                }
-            }
+#define P pair<int,int>
+// int bfs(int n,unordered_map<int,vector<int>>adj){
+//     queue<int>q;
+//     q.push(0);
+//     vector<bool>vis(n,false);
+//     vis[0]=true;
+//     int level=0;
+//     while(!q.empty()){
+//         int qsize=q.size();
+//         while(qsize--){
+//             int node=q.front();
+//             q.pop();
+//             if(node==n-1){
+//                 return level;
+//             }
+//             for(auto&ngbr:adj[node]){
+//                 if(!vis[ngbr]){
+//                     q.push(ngbr);
+//                     vis[ngbr]=true;
+//                 }
+//             }
+//         }
+//         level++;
+//     }
+//     return -1;
+// }
+//     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
+//         unordered_map<int,vector<int>>adj;
+//         for(int i=0;i<n-1;i++){
+//             adj[i].push_back(i+1);
+//         }
+//         int k=queries.size();
+//         vector<int>result(k);
+//         for(int i=0;i<k;i++){
+//             adj[queries[i][0]].push_back(queries[i][1]);
+//             result[i]=bfs(n,adj);
+
+//         }
+//         return result;
+//     }
+int dij(int n,unordered_map<int,vector<P>>adj){
+    vector<int>result(n,INT_MAX);
+    result[0]=0;
+    priority_queue<P,vector<P>,greater<P>>pq;
+    pq.push({0,0});
+    while(!pq.empty()){
+        int d=pq.top().first;
+        int node=pq.top().second;
+        pq.pop();
+
+        if(node==n-1){
+            return result[n-1];
         }
-        level++;
-    }
-    return -1;
-}
-    vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
-        unordered_map<int,vector<int>>adj;
-        for(int i=0;i<n-1;i++){
-            adj[i].push_back(i+1);
-        }
-        int k=queries.size();
-        vector<int>result(k);
-        for(int i=0;i<k;i++){
-            adj[queries[i][0]].push_back(queries[i][1]);
-            result[i]=bfs(n,adj);
+
+        if(d>result[node])
+        continue;
+
+
+        for(P &p:adj[node]){
+            int adjnode=p.first;
+            int dist=p.second;
+            if(d+dist<result[adjnode]){
+                result[adjnode]=d+dist;
+                pq.push({d+dist,adjnode});
+            }
 
         }
-        return result;
     }
-};
+    return result[n-1];
+}
+
+
+vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries){
+unordered_map<int,vector<P>>adj;
+vector<int>re;
+
+for(int i=0;i<n-1;i++){
+    adj[i].push_back({i+1,1});
+}
+for(auto &query:queries){
+    int u=query[0];
+    int v=query[1];
+    adj[u].push_back({v,1});
+    int d=dij(n,adj);
+    re.push_back(d);
+
+}
+return re;
+}
+ };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
