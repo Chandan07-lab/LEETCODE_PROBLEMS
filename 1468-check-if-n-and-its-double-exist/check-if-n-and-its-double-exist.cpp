f1de6807e02795edafe2eda4e57abd83ca4baec5
Chandan Kumar jena <116368780+Chandan07-lab@users.noreply.github.com>
2024-12-01 11:47:25 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (16.68%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+int bsearch(vector<int>&arr,int tar){
+    int l=0;
+    int r=arr.size()-1;
+    int mid;
+    while(l<=r){
+        mid=l+(r-l)/2;
+        if(arr[mid]==tar){
+            return mid;
+        }else if(arr[mid]>tar){
+            r=mid-1;
+        }else{
+            l=mid+1;
+        }   
+     }
+
+     return -1;
+}
+    bool checkIfExist(vector<int>& arr) {
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<n;i++){
+            int j=bsearch(arr,2*arr[i]);
+            if(j!=-1 && j!=i){
+                return true;
+            }
+        }
+        return false;
+    }
+};
