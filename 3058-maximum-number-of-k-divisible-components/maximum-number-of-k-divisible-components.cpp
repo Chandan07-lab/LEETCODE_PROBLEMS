3ddd1cbe9d8c5c45e30eebdfd27b5762922b4656
Chandan Kumar jena <116368780+Chandan07-lab@users.noreply.github.com>
2024-12-21 23:45:16 +0530

    Time: 107 ms (72.15%) | Memory: 173.4 MB (84.81%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
@@ -40,9 +40,9 @@ public:
 
 
 
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
+// auto init = []() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     cout.tie(nullptr);
+//     return 'c';
+// }();
