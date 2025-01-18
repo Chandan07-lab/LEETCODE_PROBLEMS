e365e1c423a9272b57d7f1387e31302936f4abe1
Chandan Kumar jena <116368780+Chandan07-lab@users.noreply.github.com>
2025-01-18 09:35:10 +0530

    Time: 22 ms (90.20%) | Memory: 60 MB (35.92%) - LeetSync

---
+++ 927-sum-of-subsequence-widths/sum-of-subsequence-widths.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int sumSubseqWidths(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int M=1e9+7;
+        long long result=0;
+        vector<int>power(n,1);
+        for(int i=1;i<n;i++){
+            power[i]=(power[i-1]*2)%M;
+        }
+        for(int i=0;i<n;i++){
+            result=(result +(long long) nums[i] *((power[i]-power[n-i-1])+M)%M)%M;
+        }
+
+        return result;
+    }
+};
