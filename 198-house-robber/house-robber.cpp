36b9ac159e919fc42f4f07c9e1e963fbb6c3af88
Chandan Kumar jena <116368780+Chandan07-lab@users.noreply.github.com>
2025-01-27 22:50:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (97.23%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,68 +1,25 @@
-// class Solution {
-// public:
-//     int func(int i, vector<int>& nums) {
-//         if (i >= nums.size()) {
-//             return 0;
-//         }
-        
-//         int robCurrent = nums[i] + func(i + 2, nums);
-//         int skipCurrent = func(i + 1, nums);
-//         return max(robCurrent, skipCurrent);
-//     }
-
-//     int rob(vector<int>& nums) {
-//       .
-//         if (nums.empty()) {
-//             return 0;
-//         }
-
-//         return func(0, nums);
-//     }
-// };
 class Solution {
 public:
+int t[101];
+int solve(int idx,vector<int>&nums){
+    if(idx==0) return nums[idx];
+
+    if(idx<0) return 0;
+
+    if(t[idx]!=-1){
+        return t[idx];
+    }
+
+    int take=nums[idx]+solve(idx-2,nums);
+    int nottake=0+solve(idx-1,nums);
+
+    return t[idx]=max(take,nottake);
+}
+
     int rob(vector<int>& nums) {
-        // Handle empty array case
-        if (nums.empty()) {
-            return 0;
-        }
-
-        // Initialize variables for dynamic programming
-        int prevMax = 0; // Maximum amount possible for robbing the previous house
-        int currMax = nums[0]; // Maximum amount possible including the current house
-
-        // Iterate through the nums vector starting from the second element
-        for (int i = 1; i < nums.size(); i++) {
-            // Calculate the maximum amount considering robbing the current house
-            int tempMax = prevMax + nums[i];
-
-            // Update the previous and current maximums
-            prevMax = currMax;
-            currMax = max(currMax, tempMax);
-        }
-
-        // Return the final maximum amount
-        return currMax;
+    memset(t,-1,sizeof(t));
+        int n=nums.size();
+        return solve(n-1,nums);
+        
     }
 };
-// class Solution {
-// public:
-//     int rob(vector<int>& nums) {
-//         int n = nums.size();
-//         if(n==0)
-//             return 0;
-//         else if(n==1) 
-//             return nums[0];
-
-//         vector<int> dp(n, 0);
-
-//         dp[0] = nums[0];
-//         dp[1] = max(nums[1], dp[0]);
-
-//         for(int i=2; i<n ; i++) {
-//             dp[i] = max(dp[i-1], nums[i]+dp[i-2]);
-//         }
-//         return dp[n-1];
-        
-//     }
-// }
