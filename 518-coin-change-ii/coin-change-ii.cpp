4e89c26593ac3fb1bfa3df761a75e0f20d04ec0d
Chandan Kumar jena <116368780+Chandan07-lab@users.noreply.github.com>
2025-02-22 11:41:10 +0530

    Time: 14 ms (79.58%) | Memory: 28.4 MB (58.35%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
+int solve(int amount,int i,vector<vector<int>>&dp,vector<int>&coins){
+    if(i==0){
+        return (amount%coins[i]==0);
+    }
+
+   if(dp[i][amount]!=-1){
+    return dp[i][amount];
+   } 
+
+    int nottake=solve(amount,i-1,dp,coins);
+    int take=0;
+
+    if(coins[i]<=amount){
+        take=solve(amount-coins[i],i,dp,coins);
+    }
+
+    return dp[i][amount]=take+nottake;
+}
 
     int change(int amount, vector<int>& coins) {
-       vector<int> dp(amount + 1, 0);
-    dp[0] = 1; // There's one way to make amount 0
-
-    // Iterate over each coin
-    for (int coin : coins) {
-        // Update the dp array for each amount from coin to amount
-        for (int i = coin; i <= amount; ++i) {
-            dp[i] += dp[i - coin];
-        }
-    }
-    
-    return dp[amount];
-
-        
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        return solve(amount,n-1,dp,coins);
     }
 };
