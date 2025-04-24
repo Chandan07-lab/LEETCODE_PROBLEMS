022d4a12f4ed7f50e7a0cd0540e04f15d305e9b6
Chandan Kumar jena <116368780+Chandan07-lab@users.noreply.github.com>
2025-04-24 22:05:16 +0530

    Time: 182 ms (27.54%) | Memory: 13.8 MB (59.33%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
+    int t[1001][1001];
+    
+    bool solve(string &s, int l, int r){
+        if(l >= r) 
+            return 1;
+
+        if(t[l][r] != -1){
+            return t[l][r];
+        }
+
+        if(s[l] == s[r]) {
+            return t[l][r] = solve(s, l+1, r-1);
+        }
+
+        return t[l][r] = false;
+    }
+    
     string longestPalindrome(string s) {
-        if (s.empty()) return "";
+        int n = s.length();
+        
+        int maxlen = INT_MIN;
+        int startingIndex = 0;
 
-        string t = "#";
-        for (char c : s) {
-            t += c;
-            t += "#";
-        }
+        memset(t, -1, sizeof(t));
 
-        int n = t.size();
-        vector<int> p(n, 0);
-        int c = 0, r = 0;
-        int maxLen = 0, centerIndex = 0;
-
-        for (int i = 0; i < n; i++) {
-            int mirror = 2 * c - i;
-            if (i < r) {
-                p[i] = min(r - i, p[mirror]); 
-            }
-            
-            while (i + p[i] + 1 < n && i - p[i] - 1 >= 0 && t[i + p[i] + 1] == t[i - p[i] - 1]) {
-                p[i]++;
-            }
-
-            if (i + p[i] > r) {
-                c = i;
-                r = i + p[i];
-            }
-
-            if (p[i] > maxLen) {
-                maxLen = p[i];
-                centerIndex = i;
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                
+                if(solve(s, i, j)) {
+                    if(j-i+1 > maxlen){
+                        startingIndex = i;
+                        maxlen = j-i+1;
+                    }
+                }
+                
             }
         }
 
-        int start = (centerIndex - maxLen) / 2;
-        return s.substr(start, maxLen);
+        return s.substr(startingIndex, maxlen);
     }
 };
